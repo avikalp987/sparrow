43d612f13ee7e2fa137c22f387ae31f65abe227e
vikalp <avikalp233@gmail.com>
2023-10-30 22:18:40 +0530

    complete the signin page

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "sparrow-eb68c.firebaseapp.com",
   projectId: "sparrow-eb68c",
   storageBucket: "sparrow-eb68c.appspot.com",
---
+++ pages/auth/signin.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {getProviders ,signIn} from "next-auth/react"
+
+export default function signin({providers}) {
+  return (
+    <div className='flex justify-center mt-20'>
+       
+       <div>
+        {Object.values(providers).map((provider) => (
+            <div className='flex flex-col items-center justify-center'>
+                <img className='w-36 object-cover rounded-full'
+                src='https://thumb.silhouette-ac.com/t/0a/0abcbe0c4d9ff728f7453070a8937dba_t.jpeg' alt="" />
+                <p className='text-center text-sm italic my-20'>This app is created for learning and testing purposes</p>
+                <button onClick={() => signIn(provider.id,{callbackUrl : "/"})}
+                className='bg-black rounded-lg p-3 text-white'>Sign in with {provider.name}</button>
+            </div>
+        ))}
+       </div>
+    </div>
+  )
+}
+
+
+export async function getServerSideProps()
+{
+    const providers = await getProviders();
+    return {
+        props:{
+            providers,
+        }
+    }
+}
